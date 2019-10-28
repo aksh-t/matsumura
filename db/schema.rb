4734dc87053cc15ddd99615204a3560d49d3f4bb
aksh-t <tanno.ao@gmail.com>
2019-10-29 01:41:36 +0900

    add User password

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_140625) do
+ActiveRecord::Schema.define(version: 2019_10_28_162836) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,28 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it 'can new User' do   
     @user = User.new( 
-      user_id: "John"
+      user_id: "John",
+      password: "aaa",
+      password_confirmation: "aaa"
     )
-    expect(@user).to be_truthy
+    expect(@user).to be_valid
+  end
+
+  it 'can authenticate' do   
+    @user = User.new( 
+      user_id: "John",
+      password: "aaa",
+      password_confirmation: "aaa"
+    )
+    expect(@user.authenticate("aaa")).to be_truthy
+  end
+
+  it 'can not authenticate' do   
+    @user = User.new( 
+      user_id: "John",
+      password: "aaa",
+      password_confirmation: "aaa"
+    )
+    expect(@user.authenticate("bbb")).to be false
   end
 end
